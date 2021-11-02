24ef3d10f782a93ec57063bb25611e5ac768d6b0
peeweemiwie <miwayagi@gmail.com>
2021-11-02 12:06:03 -0400

    Change the background gradients depends on the weather

--- src/script/weather-current.js
+++ src/script/weather-current.js
@@ -18,28 +18,35 @@ let displayCurrentWeather = (data) => {
 		inputSearch.setAttribute('value', name);
 	}
 	let headers = `
-		<h1 class="container-city text-center">${name}</h1>
-		<h2 class="container-date text-center">${currentDateTimeLong}</h2>`;
+		<h1 class="container-city">${name}</h1>
+		<h2 class="container-date">${currentDateTimeLong}</h2>`;
 
 	// change the background depends on day or night
 	let currentWeather = document.querySelector('#current-weather');
-	icon.indexOf('d') > -1
-		? currentWeather.setAttribute('data-theme', 'light')
-		: currentWeather.setAttribute('data-theme', 'dark');
+	if (icon.indexOf('n') > -1) {
+		currentWeather.setAttribute('data-theme', 'dark');
+	} else if (
+		icon.indexOf('d') > -1 &&
+		main.toLowerCase().indexOf('clear') > -1
+	) {
+		currentWeather.setAttribute('data-theme', 'light');
+	} else {
+		currentWeather.setAttribute('data-theme', 'cloudy');
+	}
 
-	let iconImg = `<img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="icon for ${description}" />`;
+	let iconImg = `<img class="img-weather" src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="icon for ${description}" />`;
 
 	let weather = `
-		<div class="px-1 major">${main}</div>
-		<div class="px-1 minor">${description}</div>`;
+		<div class="major">${main}</div>
+		<div class="minor">${description}</div>`;
 
 	let temperature = `
-		<div class="px-1 major">
+		<div class="major">
       ${Math.round(temp)}
       <span class="unit-imperial">F</span>
       <span class="unit-metric">C</span>
     </div>
-		<div class="px-1 minor">
+		<div class="minor">
       feels like
       ${Math.round(feels_like)}
       <span class="unit-imperial">F</span>
